0c5fa33eb626c86e4d09bcf00ba341a5a72db97e
Gael Rogelio Bejarano Dominguez <a.22308051281154@cbtis128.edu.mx>
2024-11-14 13:07:06 -0600

    Add files via upload

--- UIII_P1_Tabla_Materia_CRUD_v3/materia_app/views.py
+++ UIII_P1_Tabla_Materia_CRUD_v3/materia_app/views.py
@@ -16,3 +16,23 @@ def registrarMateria(request):
     ) # GUARDA EL REGISTRO
 
     return redirect("/")
+
+def seleccionarMateria(request,codigo):
+    materia = Materia.objects.get(codigo = codigo)
+    return render(request, "editarmateria.html", {"mismaterias": materia})
+
+def editarMateria(request, codigo):
+    codigo=request.POST["txtcodigo"]
+    nombre=request.POST["txtnombre"]
+    creditos=request.POST["numcreditos"]
+    materia = Materia.objects.get(codigo = codigo)
+    materia.nombre = nombre 
+    materia.creditos = creditos
+    materia.save() #guarda registro actualizado
+
+def borrarMateria(request, codigo):
+    materia = Materia.objects.get(codigo = codigo)
+    materia.delete() # Borra el registro
+    return redirect("/")
+
+
